ee00f4cdfb10948f8a2e340f9d626c79406c151b
Cris-casas-react <cristina.casas2@gmail.com>
2023-03-22 10:24:46 +0100

    ex-1-nivell-1

---
+++ src/components/Scene/Scene.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default function Scene() {
+    return (
+        <h1>El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial</h1>
+    )
+}
